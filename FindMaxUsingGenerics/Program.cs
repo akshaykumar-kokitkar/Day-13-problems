e6bf93efdecd25ca79062b628edcc56c0bcaae78
ak47 <kokitkarak@gmail.com>
2022-01-24 13:34:53 +0530

    updated UC2

--- FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/Program.cs
@@ -27,10 +27,32 @@ namespace FindMaxUsingGenerics
             }
             return Firstvalue;
         }
+        public static double MaximumFloatNumber(double Firstvalue, double secondvalue, double thirdvalue)
+        {
+            if (Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) > 0 ||
+                Firstvalue.CompareTo(secondvalue) >= 0 && Firstvalue.CompareTo(thirdvalue) > 0 ||
+                Firstvalue.CompareTo(secondvalue) > 0 && Firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return Firstvalue;
+            }
+            if (secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(Firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(Firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(Firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+            {
+                return thirdvalue;
+            }
+            return Firstvalue;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine(MaximumIntegerNumber(25, 30, 5));
-
+            Console.WriteLine(MaximumFloatNumber(25.5, 30.2, 5.3));
         }
     }
 }
